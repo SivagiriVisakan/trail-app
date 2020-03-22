b308084fa26f24385e611b163281fba090930093
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-03-22 16:30:58 +0530

    Add config files and methods to connect to the database.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-
+import config
 import auth
-
+import db
 def register_blueprints(app):
     """Register Flask blueprints."""
     app.register_blueprint(auth.blueprint)
 
 app = Flask(__name__)
 register_blueprints(app)
+app.config.from_object(config)
+app.teardown_appcontext(db.close_database_connection)
 
 @app.route('/')
 def hello_world():
