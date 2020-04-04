deee36ad88c954eb4ebad311262d307c6d551b51
Sadham-hussian <sadhamhussian2020@gmail.com>
2020-04-05 00:00:37 +0530

    populate: populate the table web event

---
+++ populate.py
@@ -0,0 +1,83 @@
+import db
+
+from faker import Faker
+
+import random
+
+faker_generate = Faker()
+
+from random import randint
+
+import json
+
+
+#fake_url = faker_generate.url()
+
+#print(fake_url)
+
+project_id = "flizon"
+
+item = ['shirt','pant','shoe','sandals','TV','AC','watch']
+
+origin_id_list = ['abcd', 'efgh', 'ijkl', 'mnop']
+
+url_names = ['/flizon/','/flizon/view_item_list','/flizon/view_item','/flizon/cart','/flizon/wish_list']
+
+event_names = [['login','signup','offers'], ['view_item'], ['add_to_cart', 'add_to_wishlist','purchase'], ['remove_from_cart','add_to_wishlist', 'purchase', 'view_item'], ['remove_from_wishlist', 'add_to_cart', 'purchase', 'view_item']]
+
+login = {}
+signup = {}
+offers = {}
+
+view_item = {"items":random.choice(item)}
+
+add_to_cart = {"value":random.randrange(100,100000,98),"currency":faker_generate.currency_code(),"items":random.choice(item)}
+
+add_to_wishlist = {"value":random.randrange(100,100000,98),"currency":faker_generate.currency_code(),"items":random.choice(item)}
+
+purchase = {"transaction_id": randint(1,100),"value":random.randrange(100,100000,98),"currency":faker_generate.currency_code(),"items":random.choice(item)}
+
+remove_from_cart = {"value":random.randrange(100,100000,98),"currency":faker_generate.currency_code(),"items":random.choice(item)}
+
+remove_from_wishlist = {"value":random.randrange(100,100000,98),"currency":faker_generate.currency_code(),"items":random.choice(item)}
+
+custom_dict = {'login': login, 'signup': signup, 'offers': offers, 'view_item': view_item, 'add_to_cart': add_to_cart, 'add_to_wishlist': add_to_wishlist, 'remove_from_cart': remove_from_cart, 'remove_from_wishlist': remove_from_wishlist, 'purchase': purchase}
+
+def populate():
+
+	for origin_id in origin_id_list:
+		user_agent = faker_generate.user_agent()
+
+		for i in range(random.randrange(20,40,2)):
+			page_url = random.choice(url_names)
+
+			event_index = url_names.index(page_url)
+
+			event_in_url = event_names[event_index]
+
+			event_type = random.choice(event_in_url)
+
+			custom_event_type = custom_dict[event_type]
+
+			custom_data = json.dumps(custom_event_type)
+
+			print(origin_id,user_agent,page_url,event_type,custom_data)
+
+			db_conn = db.get_database_connection()
+
+			with db_conn.curson() as cursor:
+				cursor.execute("INSERT INTO `user`(`project_id`,`origin_id`,`user_agent`,`page_url`,`event_type`,`custom_data`) Values (%s, %s, %s, %s, %s, %s)", (project_id, origin_id, user_agent, page_url, event_type, custom_data))
+				db_conn.commit();
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	print("populate script")
+	populate()
