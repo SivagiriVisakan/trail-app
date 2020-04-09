e7ebc36461706c339940ccc9fa7becff2a35676a
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-04-10 00:53:31 +0530

    projects: Require logged in user to acces the dashboard.

--- projects.py
+++ projects.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request, render_template
 
 import db
 from utils import check_missing_keys
+import auth
 
 blueprint = Blueprint('projects', __name__, url_prefix='/projects')
 
@@ -12,6 +13,7 @@ blueprint = Blueprint('projects', __name__, url_prefix='/projects')
 # TODO: Restrict access to projects based on user
 
 @blueprint.route('/<string:project_id>/dashboard')
+@auth.login_required
 def project_dashboard(project_id):
     return render_template('projects/home_dashboard.html')
 
