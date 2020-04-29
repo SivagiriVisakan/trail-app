de46ff31a5bc453dad8922ef7cfd0e5e03d34cd6
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-04-29 15:21:13 +0530

    (WIP) events_dashboard: Fix date range not being handled correctly.

--- organisation.py
+++ organisation.py
@@ -107,10 +107,12 @@ def project_events_dashboard(workspace, project_id):
     start_timestamp = request.args.get("start_time") or datetime.datetime.now().timestamp()
     start_timestamp = float(start_timestamp)
     start_time = datetime.datetime.utcfromtimestamp(start_timestamp)
+    start_time += datetime.timedelta(days=1)
 
     end_timestamp = request.args.get("end_time") or datetime.datetime.now().timestamp()
     end_timestamp = float(end_timestamp)
     end_time = datetime.datetime.utcfromtimestamp(end_timestamp)
+    end_time += datetime.timedelta(days=1)
 
     if 'start_time' not in request.args or 'end_time' not in request.args:
 
@@ -120,18 +122,18 @@ def project_events_dashboard(workspace, project_id):
     events_list = []
     with db_conn.cursor(cursor=None) as cursor:
         # No event type passed, so we get the details of all the events
-        sql = 'SELECT event_type FROM web_event WHERE project_id=%s GROUP BY event_type'
-        cursor.execute(sql, (project_id,))
+        sql = 'SELECT event_type FROM web_event WHERE project_id=%s AND DATE(time_entered) >= DATE(%s) AND DATE(time_entered) <= DATE(%s) GROUP BY event_type'
+        cursor.execute(sql, (project_id, start_time.isoformat(), end_time.isoformat()))
         result = cursor.fetchall()
         events_list = [row["event_type"] for row in result]
 
-    data = get_event_details(project_id, event_type)
+    data = get_event_details(project_id, start_time, end_time, event_type)
 
     return render_template('projects/events_dashboard.html', template_context={"project_id": project_id, "workspace": workspace,
                                 "event_data": data, "events_list": events_list,"start_date": start_time, "end_date": end_time})
 
 
-def get_event_details(project_id, event_type=None):
+def get_event_details(project_id, start_time, end_time, event_type=None):
     """
 
     """
@@ -142,26 +144,27 @@ def get_event_details(project_id, event_type=None):
     with db_conn.cursor(cursor=None) as cursor:
         if event_type is None:
             # No event type passed, so we get the details of all the events
-            sql = 'SELECT event_type, count(*) as total_events_count FROM web_event WHERE project_id=%s GROUP BY event_type'
-            cursor.execute(sql, (project_id,))
+            sql = 'SELECT event_type, count(*) as total_events_count FROM web_event WHERE project_id=%s AND DATE(time_entered) >= DATE(%s)  AND DATE(time_entered) <= DATE(%s) GROUP BY event_type'
+            cursor.execute(sql, (project_id, start_time.isoformat(), end_time.isoformat()))
         else:
-            sql = 'SELECT event_type, count(*) as total_events_count FROM web_event WHERE project_id=%s AND event_type=%s'
-            cursor.execute(sql, (project_id, event_type))
+            sql = 'SELECT event_type, count(*) as total_events_count FROM web_event WHERE project_id=%s AND event_type=%s AND DATE(time_entered) >= DATE(%s)  AND DATE(time_entered) <= DATE(%s) '
+            cursor.execute(sql, (project_id, event_type, start_time.isoformat(), end_time.isoformat()))
         result = cursor.fetchall()
         result_to_return = result # The DictCursor we use returns the data in required format
+        print('\n\n\n',cursor._last_executed,'\n\n\n')
         
         for event_dict in result_to_return:
             # Query the page_wise events count
             sql = 'SELECT page_url, count(*) as total_events_count FROM web_event WHERE project_id=%s  \
-                                                AND event_type=%s GROUP BY page_url'
-            cursor.execute(sql, (project_id, event_dict["event_type"]))
+                                                AND event_type=%s AND DATE(time_entered) >= DATE(%s)  AND DATE(time_entered) <= DATE(%s) GROUP BY page_url'
+            cursor.execute(sql, (project_id, event_dict["event_type"], start_time.isoformat(), end_time.isoformat()))
             result = cursor.fetchall()
             event_dict["page_wise"] = result
 
 
             resulting_custom_data_keys = []
-            sql = 'SELECT JSON_KEYS(custom_data)  as custom_keys from web_event where event_type=%s group by custom_keys'
-            cursor.execute(sql, ( event_dict["event_type"], ))
+            sql = 'SELECT JSON_KEYS(custom_data)  as custom_keys from web_event where event_type=%s AND DATE(time_entered) >= DATE(%s)  AND DATE(time_entered) <= DATE(%s) group by custom_keys'
+            cursor.execute(sql, ( event_dict["event_type"], start_time.isoformat(), end_time.isoformat()))
             result = cursor.fetchall()
             for row in result:
                 resulting_custom_data_keys.extend(json.loads(row["custom_keys"]))
@@ -171,9 +174,9 @@ def get_event_details(project_id, event_type=None):
             custom_data_key_value_aggregation = {}
             for key in resulting_custom_data_keys:
                 sql = 'SELECT JSON_EXTRACT(custom_data, %s) as key_value, count(*) as key_value_count from \
-                        web_event where event_type=%s group by key_value'
+                        web_event where event_type=%s AND DATE(time_entered) >= DATE(%s)  AND DATE(time_entered) <= DATE(%s)  group by key_value'
                 json_path_for_mysql = f'$.{key}'
-                cursor.execute(sql, (json_path_for_mysql, event_dict["event_type"]))
+                cursor.execute(sql, (json_path_for_mysql, event_dict["event_type"], start_time.isoformat(), end_time.isoformat()))
 
                 result = cursor.fetchall()
                 custom_data_key_value_aggregation[key] = result
