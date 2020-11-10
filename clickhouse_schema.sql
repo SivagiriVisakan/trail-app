8fa8c62fede21e26e774eda7000fcec6b608a666
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-11-10 12:06:08 +0530

    Change the schema of the events in ClickHouse.

--- clickhouse_schema.sql
+++ clickhouse_schema.sql
@@ -1,14 +1,21 @@
 CREATE TABLE `web_events` (
   `session_id` UInt64 NOT NULL,
+  `project_id` 
+  `origin_user_id` String NOT NULL,
   `time_entered` DateTime NOT NULL,
   `user_agent` String NOT NULL,
   `page_url` String,
   `page_domain` String,
   `page_params` String,
   `event_type` FixedString(50) NOT NULL,
-  `custom_data` String NOT NULL
-) ENGINE=MergeTree() ORDER BY (page_domain, page_url);
-
-
--- To insert an event
--- INSERT into web_events (session_id, time_entered, user_agent, page_url, page_domain, page_params, event_type, custom_data) VALUES (1, now() , 'user_agent', '/home', 'example.com', '{}', 'pageview', '{}');
+  `browser` String,
+  `os` String,
+  `device` String,
+  `referrer` String,
+  `utm_campaign` String,
+  `custom_data` NESTED 
+  (
+    `key` String
+    `value` String
+  )
+) ENGINE=MergeTree() ORDER BY (page_domain, time_entered, session_id);
